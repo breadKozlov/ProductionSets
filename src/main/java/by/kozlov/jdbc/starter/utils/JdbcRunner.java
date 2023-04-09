4c983103bcb940f4c01ee7045a1fe49bf5ab2b10
breadKozlov <killersarequiet@gmail.com>
2023-04-09 21:52:10 +0300

    Add Set and it dao

--- src/main/java/by/kozlov/jdbc/starter/utils/JdbcRunner.java
+++ src/main/java/by/kozlov/jdbc/starter/utils/JdbcRunner.java
@@ -1,9 +1,6 @@
 package by.kozlov.jdbc.starter.utils;
 
-import by.kozlov.jdbc.starter.dao.BrigadeDao;
-import by.kozlov.jdbc.starter.dao.MaterialDao;
-import by.kozlov.jdbc.starter.dao.ShiftHourDao;
-import by.kozlov.jdbc.starter.dao.WorkerDao;
+import by.kozlov.jdbc.starter.dao.*;
 import by.kozlov.jdbc.starter.entity.Material;
 
 public class JdbcRunner {
@@ -14,6 +11,7 @@ public class JdbcRunner {
         var shiftHourDao = ShiftHourDao.getInstance();
         var brigadeDao = BrigadeDao.getInstance();
         var workerDao = WorkerDao.getInstance();
+        var setDao = SetDao.getInstance();
 
         System.out.println(materialDao.findAll());
         System.out.println(materialDao.findById(2));
@@ -22,6 +20,8 @@ public class JdbcRunner {
         System.out.println(workerDao.findAll());
         System.out.println(brigadeDao.findAll());
         System.out.println(shiftHourDao.findAll());
+        System.out.println(setDao.findAll());
+        System.out.println(setDao.findById(2));
 
     }
 }
