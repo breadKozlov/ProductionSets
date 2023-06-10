d4003b45c0e2263d1d5b02a5cffc979af0e41e7b
breadKozlov <killersarequiet@gmail.com>
2023-06-10 16:13:21 +0300

    Add tests of page and sort in service

---
+++ src/test/java/by/kozlov/annotation/IT.java
@@ -0,0 +1,20 @@
+package by.kozlov.annotation;
+
+import by.kozlov.TestApplicationRunner;
+import by.kozlov.spring.ApplicationRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = {TestApplicationRunner.class, ApplicationRunner.class})
+@Transactional
+@Sql({"/sql/init.sql","/sql/test.sql"})
+public @interface IT {
+}
---
+++ src/test/java/by/kozlov/integration/service/MaterialsProductionIT.java
@@ -0,0 +1,45 @@
+package by.kozlov.integration.service;
+
+import by.kozlov.annotation.IT;
+import by.kozlov.spring.dto.MaterialsProductionDto;
+import by.kozlov.spring.service.MaterialsProductionService;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@IT
+@RequiredArgsConstructor
+public class MaterialsProductionIT {
+
+    private static final Integer BRIGADE_ID = 1;
+
+    private final MaterialsProductionService materialsProductionService;
+
+    @Test
+    void findAllByPageAndSortByDateOfProduction() {
+
+        var page = PageRequest.of(0,
+                4,
+                Sort.by("dateOfProduction"));
+        var production = materialsProductionService.findAll(page);
+
+        assertThat(production).hasSize(4);
+
+        var datesOfProduction = production.stream().map(it -> it.getDateOfProduction().toString()).toList();
+        assertThat(datesOfProduction).contains("2023-01-03", "2023-01-05", "2023-02-06", "2023-03-02");
+    }
+
+    @Test
+    void findAllByBrigadeIdAndByPageAndSortByDateOfProduction() {
+
+        var page = PageRequest.of(2,2,Sort.by("dateOfProduction").descending());
+        var production = materialsProductionService.findAllByBrigadeId(BRIGADE_ID,page);
+
+        assertThat(production).hasSize(1);
+        var quantities = production.stream().map(MaterialsProductionDto::getQuantity).toList();
+        assertThat(quantities).contains(54.0);
+    }
+}
