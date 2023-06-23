116bd785f9af891cd197da5e231bfdb4e0f4fed8
breadKozlov <killersarequiet2018@gmail.com>
2023-06-23 11:59:41 +0300

    Add some functional in production controller

--- src/main/java/by/kozlov/spring/dto/ProductionCreateEditDto.java
+++ src/main/java/by/kozlov/spring/dto/ProductionCreateEditDto.java
@@ -1,12 +1,8 @@
 package by.kozlov.spring.dto;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Value;
-import lombok.experimental.FieldNameConstants;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -20,7 +16,7 @@ public class ProductionCreateEditDto {
     Integer setId;
     @NotNull
     Integer madeSets;
-    //@NotEmpty
+    @NotNull
     //@DateTimeFormat(pattern = "dd-MM-yyyy")
     LocalDate dateOfProduction;
 }
--- src/main/java/by/kozlov/spring/http/controller/ProductionController.java
+++ src/main/java/by/kozlov/spring/http/controller/ProductionController.java
@@ -5,6 +5,7 @@ import by.kozlov.spring.dto.ProductionCreateEditDto;
 import by.kozlov.spring.dto.UserReadDto;
 import by.kozlov.spring.service.ProductionService;
 import by.kozlov.spring.service.SetService;
+import by.kozlov.spring.service.WorkerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/production")
@@ -24,6 +26,7 @@ public class ProductionController {
 
     private final ProductionService productionService;
     private final SetService setService;
+    private final WorkerService workerService;
 
 
     @GetMapping
@@ -32,6 +35,7 @@ public class ProductionController {
             var page = PageRequest.of(0,
                     100,
                     Sort.by("dateOfProduction"));
+            model.addAttribute("flag", "flag");
             model.addAttribute("production",productionService.findAll(page));
             return "admin/adminProduction";
         } else {
@@ -40,30 +44,51 @@ public class ProductionController {
     }
 
     @GetMapping("/{id}/update")
-    public String update(@PathVariable("id") Integer id, Model model) {
+    public String update(@PathVariable("id") Integer id, Model model,
+                         @ModelAttribute("flag") String flag) {
 
         var product = productionService.findById(id).orElseThrow();
         var kits = setService.findAll();
         model.addAttribute("product",product);
         model.addAttribute("kits",kits);
-        return "/admin/adminEditWorkerProduction";
+        if (flag != null) {
+            var workers = workerService.findAll();
+            model.addAttribute("workers",workers);
+            model.addAttribute("flag",flag);
+            return "admin/adminEditProduction";
+        } else {
+            return "admin/adminEditWorkerProduction";
+        }
     }
 
     @PostMapping("/{id}/update")
-    public String update(@PathVariable("id") Integer id, @ModelAttribute("product") ProductionCreateEditDto product) {
+    public String update(@PathVariable("id") Integer id,
+                         @ModelAttribute("product") ProductionCreateEditDto product,
+                         @ModelAttribute("flag") String flag) {
+
         return productionService.update(id,product)
-                .map(it -> "redirect:/admins/worker/" + product.getWorkerId())
+                .map(it -> {
+                    if (flag != null) {
+                        return "redirect:/production";
+                    } else {
+                         return "redirect:/admins/worker/" + product.getWorkerId();
+                        }
+                })
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/{id}/delete")
-    public String delete(@PathVariable("id") Integer id) {
+    public String delete(@PathVariable("id") Integer id, @ModelAttribute("flag") String flag) {
 
         var workerId = productionService.findById(id).orElseThrow().getWorker().getId();
         if(!productionService.delete(id)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
-        return "redirect:/admins/worker/" + workerId;
+        if (flag != null) {
+            return "redirect:/production";
+        } else {
+            return "redirect:/admins/worker/" + workerId;
+        }
     }
 
     @GetMapping("{id}/createProductForWorker")
@@ -73,12 +98,29 @@ public class ProductionController {
         model.addAttribute("kits",kits);
         model.addAttribute("workerId",id);
         model.addAttribute("product",product);
-        model.addAttribute("localDate", LocalDate.now());
-        return "/admin/adminCreateWorkerProduction";
+        return "admin/adminCreateWorkerProduction";
     }
 
     @PostMapping("/createProductForWorker")
     public String createProductForWorker(@ModelAttribute ProductionCreateEditDto product) {
         return "redirect:/admins/worker/" + productionService.create(product).getWorker().getId();
+    }
+
+    @GetMapping("/createProduct")
+    public String createProduct(Model model, @ModelAttribute ProductionCreateEditDto product) {
+
+        var workers = workerService.findAll();
+        var kits = setService.findAll();
+        model.addAttribute("workers",workers);
+        model.addAttribute("kits",kits);
+        model.addAttribute("product",product);
+        return "admin/adminCreateProduction";
+
+    }
+
+    @PostMapping("/createProduct")
+    public String createProduct(@ModelAttribute ProductionCreateEditDto product) {
+        productionService.create(product);
+        return "redirect:/production";
     }
 }
