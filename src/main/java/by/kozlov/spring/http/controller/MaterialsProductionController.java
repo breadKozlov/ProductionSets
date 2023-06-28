376a87b4a6456525dd5e5b750e4c68363a21b448
breadKozlov <killersarequiet2018@gmail.com>
2023-06-28 16:10:48 +0300

    Add view materials production in admin

--- src/main/java/by/kozlov/spring/http/controller/MaterialsProductionController.java
+++ src/main/java/by/kozlov/spring/http/controller/MaterialsProductionController.java
@@ -1,18 +1,17 @@
 package by.kozlov.spring.http.controller;
 
+import by.kozlov.spring.dto.UserReadDto;
 import by.kozlov.spring.service.BrigadeService;
 import by.kozlov.spring.service.MaterialService;
 import by.kozlov.spring.service.MaterialsProductionService;
 import by.kozlov.spring.service.WorkerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/materialsProduction")
@@ -23,6 +22,13 @@ public class MaterialsProductionController {
     private final MaterialsProductionService materialsProductionService;
     private final WorkerService workerService;
 
+    @GetMapping
+    public String findAll(Model model) {
+        var page = PageRequest.of(0,100, Sort.by("dateOfProduction"));
+        var materials = materialsProductionService.findAll(page);
+        model.addAttribute("materials",materials);
+        return "admin/adminMaterialsProduction";
+    }
     @GetMapping("/{id}")
     public String findById(@PathVariable("id") Integer id, Model model) {
 
@@ -32,5 +38,23 @@ public class MaterialsProductionController {
         model.addAttribute("worker",worker);
         model.addAttribute("materials",materials);
         return "worker/workerReleaseReport";
+    }
+
+    @GetMapping("/{id}/update")
+    public String update(@PathVariable("id") Integer id,
+                         @ModelAttribute("user") UserReadDto user){
+
+        return "admin/adminEditMaterialsProduction";
+    }
+
+    @PostMapping("/{id}/update")
+    public String update(@PathVariable("id") Integer id) {
+        return "redirect:/materialsProduction";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String delete(@PathVariable("id") Integer id) {
+
+        return "view";
     }
 }
